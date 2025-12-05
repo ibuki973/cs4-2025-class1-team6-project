f7a36491dc613ca1898606a6f85c381a5d0eb69c
s1f102401348 <s1f102401348@iniad.org>
2025-12-05 22:20:01 +0900

    staticファイルを作成しました

---
+++ team6/static/js/game1.js
@@ -0,0 +1,9 @@
+//game1.js: ゲーム1用のJavaScript
+
+console.log("game1.js loaded")
+
+//関数
+
+function startGame1() {
+    //Todo:
+}
---
+++ team6/static/js/game2.js
@@ -0,0 +1,9 @@
+//game2.js: ゲーム2用のJavaScript
+
+console.log("game2.js loaded")
+
+//関数
+
+function startGame2() {
+    //Todo:
+}
