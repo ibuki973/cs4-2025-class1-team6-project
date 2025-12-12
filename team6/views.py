7dca7f81771851205298443369c524bb8e028f2c
1F10240189 <s1f102401898@iniad.org>
2025-12-12 16:00:00 +0900

    d

--- team6/views.py
+++ team6/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-<<<<<<< HEAD
 # Create your views here.
 
 def tictactoe_game(request, room_name="lobby"):
@@ -13,7 +12,3 @@ def tictactoe_game(request, room_name="lobby"):
     }
     # room_nameをコンテキストとして渡し、テンプレートをレンダリング
     return render(request, 'team6/Tic-Tac-Toe.html', context)
-=======
-def home(request):
-    return HttpResponse("TEAM6: 動作確認OK")
->>>>>>> 3b251b33935677b1357414ccb7515d3bdbd3a351
