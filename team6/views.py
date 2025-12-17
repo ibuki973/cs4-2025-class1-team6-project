d15bb94c8f8b4ada01c1e265bae934f930180c3a
1F10240189 <s1f102401898@iniad.org>
2025-12-17 15:04:29 +0900

    三目並べのオフライン対戦

--- team6/views.py
+++ team6/views.py
@@ -1,26 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def tictactoe_menu(request):
+    """メニュー画面（ルール説明とモード選択）"""
+    return render(request, 'team6/tictactoe_menu.html')
+
+def tictactoe_offline(request):
+    """オフライン対戦モード"""
+    return render(request, 'team6/tictactoe_offline.html')
 
 def tictactoe_game(request, room_name="lobby"):
-    """
-    三目並べゲームページをレンダリングする
-    """
-    context = {
-        'room_name': room_name
-    }
-    # room_nameをコンテキストとして渡し、テンプレートをレンダリング
+    """オンライン対戦モード"""
+    context = {'room_name': room_name}
     return render(request, 'team6/Tic-Tac-Toe.html', context)
-
-#ログインしていないとき自動で/login/に飛ばす
 
 def dashboard(request):
     return render(request, 'team6/dashboard.html')
-
-def tictactoe_menu(request):
-    """
-    三目並べのメニュー画面（ルール説明とモード選択）を表示
-    """
-    return render(request, 'team6/tictactoe_menu.html')
